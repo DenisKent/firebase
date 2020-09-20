ffd0311e18de8cfa46fdcfb638c2ca4a973d35d2
Denis Kent <denis@hekahappy.com>
2020-09-20 19:09:36 +0100

    checking cicd

--- app/core/App.jsx
+++ app/core/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
     return (
         <div>
-            <h1>Hello world 3</h1>
+            <h1>Hello world 4</h1>
         </div>
     )
 }
